6bcd69aef2879a3193bd586c291c2bf3edbadf3a
hazim1093 <hazim_malik@hotmail.com>
2017-12-27 14:23:40 +0500

    Add sleep

--- scripts/put-template.sh
+++ scripts/put-template.sh
@@ -8,7 +8,7 @@ TEMPLATE_NAME=${TEMPLATE_NAME:-"logs"}
 
 # Wait for ES to start up properly
 until $(curl -s -f -o /dev/null --connect-timeout 1 -m 1 --head ${ES_URL}) ; do
-    sleep 0.1;
+    sleep 0.5;
     retries=$(($retries-1))
     
     if [[ ${retries} -eq 0 ]]; 
@@ -22,3 +22,6 @@ done
 if ! [ $(curl -s -f -o /dev/null ${ES_URL}/_template/${TEMPLATE_NAME}) ]; then
     curl -XPUT -d@/es-template/config/${TEMPLATE_FILE_NAME} -H 'Content-Type: application/json' ${ES_URL}/_template/${TEMPLATE_NAME}?pretty
 fi
+
+# Sleep 1m so that kubernetes can mark this as healthy
+sleep 1m;
